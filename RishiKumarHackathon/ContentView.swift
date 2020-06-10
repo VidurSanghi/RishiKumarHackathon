f34c808db8804ba5b0170d927002b141593c8da7
VidurSanghi <52296159+VidurSanghi@users.noreply.github.com>
2020-06-09 23:26:55 -0700

    Initial Commit

---
+++ RishiKumarHackathon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RishiKumarHackathon
+//
+//  Created by Vidur Sanghi on 6/9/20.
+//  Copyright © 2020 ManResa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
